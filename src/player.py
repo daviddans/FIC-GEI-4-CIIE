eeca90c0e3edf57f73710bd9a4e97129a28a2484
alejandragachomediavilla <alejandragachomediavilla@gmail.com>
2026-02-22 18:26:11 +0100

    player refactorizado

--- src/player.py
+++ src/player.py
@@ -9,33 +9,21 @@ class Player(abstract.Object):
         self.pos = (0,0)
         self.rect = pygame.Rect()
         self.rect.topleft = self.pos
+
         self.sprite = components.Grapics(self.rect)
         self.sprite.addAnimation("first", "player-base.png")
         self.sprite.playAnimation("first")
+        self.input = components.Input(self)
+        self.move = components.Movement(self, speed=300)
         self.sprite.update(0)
-    def input(self):
-        keys = pygame.key.get_pressed()
-        vector = (0,0)
-        if keys[pygame.K_w]:
-            vector = (0,-1)
-        if keys[pygame.K_s]:
-            vector = (0,1)
-        if keys[pygame.K_a]:
-            vector = (-1,0) 
-        if keys[pygame.K_d]:
-            vector = (1,0)
-        return vector
 
-    def move(self, vector):
-        self.pos = (self.pos[0]+vector[0], self.pos[1]+vector[1])
-        self.rect.topleft = self.pos
     def collide(self):
         pass
 
     def update(self, dt):
-        vector = self.input()
-        vector = (vector[0]* dt, vector[1]*dt)
-        self.move(vector)
+        self.input.update()
+        vector = self.input.get_vector()
+        self.move.update(vector, dt)
         self.sprite.update(dt)
 
     def events(self):
